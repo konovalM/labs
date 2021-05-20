275ace1c05dfad8d749dd93a94677ba431e43c12
Коноваленко Максим <71011637+konovalM@users.noreply.github.com>
2021-05-20 16:08:09 +0300

    Add files via upload

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -1,36 +1,59 @@
-from bottle import (
-    route, run, template, request, redirect
-)
+from bottle import debug, redirect, request, route, run, template
 
-from scrapper import get_news
-from db import News, session
 from bayes import NaiveBayesClassifier
+from db import News, session
+from scraputils import get_news
 
 
 @route("/news")
 def news_list():
+    debug(True)
     s = session()
     rows = s.query(News).filter(News.label == None).all()
-    return template('news_template', rows=rows)
+    return template("news_template", rows=rows)
 
 
 @route("/add_label/")
 def add_label():
-    # PUT YOUR CODE HERE
+    label = request.query["label"]
+    #print(request.query)
+    id = request.query["id"]
+    #print(label)
+    #print(id)
+    print(label, id)
+    s = session()
+    page = s.query(News).get(id)
+    print(page.label)
+    page.label = label
+    print(page.label)
+    s.add(page)
+    s.commit()
+    print(page.title)
     redirect("/news")
 
 
 @route("/update")
 def update_news():
-    # PUT YOUR CODE HERE
+    s = session()
+    test = get_news("https://news.ycombinator.com/newest", 2)
+    for i in range(len(test)):
+        if len(s.query(News).filter(News.title == test[i]['title']).filter(News.author == test[i]['author']).all()) == 0:
+            news = News(title=str(test[i]['title']), 
+                    author=str(test[i]['author']),
+                    url=str(test[i]['url']),
+                    comments=int(test[i]['comments']),
+                    points=int(test[i]['points']))
+            s.add(news)
+            s.commit()
     redirect("/news")
 
 
 @route("/classify")
 def classify_news():
     # PUT YOUR CODE HERE
+    pass
 
 
 if __name__ == "__main__":
-    run(host="localhost", port=8080)
-
+    run(reloader=True)
+    
